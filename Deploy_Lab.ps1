051f2b41fa997e5506abd194815c20142009c7bd
Patrick Naegele <patrickn237@gmail.com>
2021-04-19 11:20:08 -0400

    added variables

--- Deploy_Lab.ps1
+++ Deploy_Lab.ps1
@@ -1,6 +1,8 @@
 Connect-AzAccount
-New-AzResourceGroup -Name "PowerShell-lab" -Location "East US"
+$rgname = "PowerShell-lab"
+$location = "East US"
+New-AzResourceGroup -Name $rgname -Location $location
 
-New-AzResourceGroupDeployment -Name Testdeploy -ResourceGroupName PowerShell-lab -TemplateFile "C:\Users\Patrick\Documents\Microsoft\PowerShell Learning\Deploy Computer Lab\vm-01_template.json" -TemplateParameterFile "C:\Users\Patrick\Documents\Microsoft\PowerShell Learning\Deploy Computer Lab\vm-01_parameters.json"
+New-AzResourceGroupDeployment -Name Testdeploy -ResourceGroupName $rgname -TemplateFile "C:\Users\Patrick\Documents\Microsoft\PowerShell Learning\Deploy Computer Lab\vm-01_template.json" -TemplateParameterFile "C:\Users\Patrick\Documents\Microsoft\PowerShell Learning\Deploy Computer Lab\vm-01_parameters.json"
 
-New-AzResourceGroupDeployment -Name Testdeploy -ResourceGroupName PowerShell-lab -TemplateFile "C:\Users\Patrick\Documents\Microsoft\PowerShell Learning\Deploy Computer Lab\vm-02_template.json" -TemplateParameterFile "C:\Users\Patrick\Documents\Microsoft\PowerShell Learning\Deploy Computer Lab\vm-02_parameters.json"
+New-AzResourceGroupDeployment -Name Testdeploy -ResourceGroupName $rgname -TemplateFile "C:\Users\Patrick\Documents\Microsoft\PowerShell Learning\Deploy Computer Lab\vm-02_template.json" -TemplateParameterFile "C:\Users\Patrick\Documents\Microsoft\PowerShell Learning\Deploy Computer Lab\vm-02_parameters.json"
